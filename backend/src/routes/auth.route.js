8e5cbf066f377542c0167f3bb60f2ff78bd6841f
nedim <amajoudnedim@gmail.com>
2025-02-04 16:14:58 +0100

    creation de la bdd et du backend encore en construction

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,13 @@
+// @ts-nocheck
+import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+
+router.post("/login", login);
+
+router.post("/logout", logout);
+
+export default router;
