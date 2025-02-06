79768f391f108005a15370644f1a16dc4772663f
nedim <amajoudnedim@gmail.com>
2025-02-06 17:29:41 +0100

    fin du backend il restera socket io a mettre en place

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
@@ -12,6 +13,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 app.listen(PORT, () => {
   console.log(`server runing on PORT : ${PORT}`);
   connectDB();
