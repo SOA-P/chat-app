f79e895d9854e365ac67bc0ea86717a8322f7463
nedim <amajoudnedim@gmail.com>
2025-02-07 16:42:38 +0100

    création des routes et un peu de logique pour la connexion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,49 @@
+// @ts-nocheck
+import NavBar from "./components/NavBar.jsx";
+import HomePage from "./components/HomePage";
+import LoginPage from "./components/LoginPage.jsx";
+import SignUpPage from "./components/SignUpPage";
+import ProfilPage from "./components/ProfilPage.jsx";
+import SettingsPage from "./components/SettingsPage.jsx";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useAuthStore } from "./store/useAuthStore.jsx";
+import { useEffect } from "react";
+import { Loader } from "lucide-react";
 const App = () => {
-  return <div className="text-red-500">HELLO WORLD</div>;
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+  console.log({ authUser });
+
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+  return (
+    <div>
+      <NavBar />
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route
+          path="/profil"
+          element={authUser ? <ProfilPage /> : <Navigate to="/login" />}
+        />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,14 @@
 import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
 import "./main.css";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 // @ts-ignore
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
